aa334d8dc5294a7b362232f408eec159177f1d03
Abduqodir65 <abduqodirgithub@gmail.com>
2024-12-17 16:33:02 +0500

    'ok'

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
-import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import Login from "./pages/Login";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Header from "./components/Header";
-import UserPage from "./pages/UserPage";
+import ChallengeTracker from "./components/MainComponent"
 
-export default function App() {
+function App() {
+
   return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route element={<ProtectedRoute />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/user/:userId" element={<UserPage />} />
-        </Route>
-      </Routes>
+    <div className="gird place-items-center">
+     
+    <ChallengeTracker />
     </div>
-  );
+  )
 }
+
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}"
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
