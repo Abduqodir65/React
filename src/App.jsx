1d434857d832c4eff30b360b075fccb3d06a7e0c
Abduqodir65 <abduqodirgithub@gmail.com>
2024-12-20 13:42:10 +0500

    'ok'

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./store/store";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import FlightBooking from "./pages/FlightBooking";
-import Login from "./pages/Login";
-import Navbar from "./components/navbar";
-import ProtectedRoute from "./components/ProtectedRoute"
-import Profile from "./pages/Profile";
-import OrdersList from "./pages/OrdersList";
-
-// Pages
-// import FlightBooking from "./pages/FlightBooking";
-// import Login from "./pages/Login";
-// import OrdersList from "./pages/OrdersList";
-
-// import ProtectedRoute from "./components/ProtectedRoute";
-// import Navbar from "./components/NavBar";
-// import UserProfile from "./pages/UserProfile";
+import React from 'react'
+import Navbar from './components/Navbar'
+import { BrowserRouter, Route, Router, Routes } from 'react-router'
+import Home from './pages/Home'
+import TodoList from './pages/TodosList'
+import { login } from './services/login.service'
 
 const App = () => {
+  let loginRes = login()
+  console.log(loginRes);
+  
   return (
-    <Provider store={store}>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<FlightBooking/>} />
-            <Route path="/login"  element={<Login/>}/>
-            <Route
-              path='/orders'
-              element={
-                <ProtectedRoute>
-                  <OrdersList/>
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path='/profile'
-              element={
-                <ProtectedRoute>
-                  <Profile />
-                </ProtectedRoute>
-              }
-            />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/todos" element={<TodoList/>} />
+        </Routes>
+      </BrowserRouter>
+  )
+}
 
-           </Routes>
-          <ToastContainer />
-        </Router>
-      </LocalizationProvider>
-    </Provider>
-  );
-};
-
-export default App;
+export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,138 +1,54 @@
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link as RouterLink, useNavigate } from "react-router-dom";
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Button,
-  Box,
-  Avatar,
-  Menu,
-  MenuItem,
-  IconButton,
-  Divider,
-} from "@mui/material";
-import {
-  Home as HomeIcon,
-  FlightTakeoff as FlightIcon,
-  Logout as LogoutIcon,
-  AccountCircle as ProfileIcon,
-} from "@mui/icons-material";
-
-import { logout } from "../store/authSlice";
+import { NavLink } from "react-router";
 
 const Navbar = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const { isAuthenticated, user } = useSelector((state) => state.auth);
-  const [anchorEl, setAnchorEl] = useState(null);
-
-  const handleMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-  };
-
-  const handleLogout = () => {
-    dispatch(logout());
-    handleMenuClose();
-    navigate("/login");
-  };
-
-  return (
-    <AppBar position='static' color='primary' elevation={2}>
-      <Toolbar sx={{ justifyContent: "space-between" }}>
-        <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
-          <FlightIcon sx={{ fontSize: 32 }} />
-          <Typography
-            variant='h5'
-            component={RouterLink}
-            to='/'
-            sx={{
-              color: "white",
-              textDecoration: "none",
-              fontWeight: "bold",
-            }}
-          >
-            SkyLink
-          </Typography>
-        </Box>
-
-        <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
-          {isAuthenticated ? (
-            <>
-              <Button
-                color='inherit'
-                component={RouterLink}
-                to='/profile'
-                startIcon={<HomeIcon />}
-              >
-                Profile
-              </Button>
-              <Button
-                color='inherit'
-                component={RouterLink}
-                to='/orders'
-                startIcon={<FlightIcon />}
-              >
-                My Bookings
-              </Button>
-
-              <IconButton onClick={handleMenuOpen} color='inherit'>
-                <Avatar
-                  src={user?.image || undefined}
-                  alt={user?.username}
-                  sx={{
-                    width: 40,
-                    height: 40,
-                    border: "2px solid white",
-                  }}
-                >
-                  {user?.username?.charAt(0).toUpperCase()}
-                </Avatar>
-              </IconButton>
-
-              <Menu
-                anchorEl={anchorEl}
-                open={Boolean(anchorEl)}
-                onClose={handleMenuClose}
-                transformOrigin={{ horizontal: "right", vertical: "top" }}
-                anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-              >
-                <MenuItem disabled>
-                  <Box>
-                    <Typography variant='subtitle1' fontWeight='bold'>
-                      {user?.username}
-                    </Typography>
-                    <Typography variant='body2' color='text.secondary'>
-                      {user?.email}
-                    </Typography>
-                  </Box>
-                </MenuItem>
-                <Divider />
-                <MenuItem onClick={handleLogout}>
-                  <LogoutIcon sx={{ mr: 2 }} />
-                  Logout
-                </MenuItem>
-              </Menu>
-            </>
-          ) : (
-            <Button
-              color='inherit'
-              component={RouterLink}
-              to='/login'
-              startIcon={<ProfileIcon />}
-            >
-              Login
-            </Button>
-          )}
-        </Box>
-      </Toolbar>
-    </AppBar>
-  );
+    return (
+        <div className='navbar bg-base-100 shadow-md mb-8'>
+            <div className='navbar-start'>
+                <div className='dropdown'>
+                    <label tabIndex={0} className='btn btn-ghost sm:hidden'>
+                        <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            className='h-5 w-5'
+                            fill='none'
+                            viewBox='0 0 24 24'
+                            stroke='currentColor'
+                        >
+                            <path
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
+                                strokeWidth='2'
+                                d='M4 6h16M4 12h8m-8 6h16'
+                            />
+                        </svg>
+                    </label>
+                    <ul
+                        tabIndex={0}
+                        className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52'
+                    >
+                        <li>
+                            <NavLink to='/'>Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/todos'>Todos</NavLink>
+                        </li>
+                    </ul>
+                </div>
+                <NavLink to='/' className='btn btn-ghost normal-case text-xl'>
+                    Todo App
+                </NavLink>
+            </div>
+            <div className='navbar-end hidden sm:flex'>
+                <ul className='menu menu-horizontal px-1'>
+                    <li>
+                        <NavLink to='/'>Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/todos'>Todos</NavLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;
